6963f7bf12240721beb86cbd85897403a71ce89f
Caleb Darling <calebd@marcatopartners.com>
2023-01-17 23:51:07 -0600

    Create ASCII-Art.cs

---
+++ ASCII-Art.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      // This program will output text to the console and create ASCII art.
+      Console.WriteLine(" .-.");
+      Console.WriteLine("(o o)");
+      Console.WriteLine("| O |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("Greg the Ghost");
+     }
+  }
+}
